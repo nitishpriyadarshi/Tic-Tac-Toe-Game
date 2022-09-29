c4010d63060ed48ac549e0b47d8227d63a981ffe
nitis <nitish8152@gmail.com>
2022-09-29 15:54:49 +0530

    TicTacToeBoardCreated

--- src/com/briddgelab/TicTacToeGame.java
+++ src/com/briddgelab/TicTacToeGame.java
@@ -1,7 +1,26 @@
 package com.briddgelab;
-
+//Class to create ticktactoe game with different methods//
 public class TicTacToeGame {
-    public static void main(String[] args) {
-        System.out.println("---Welcome To TicTacToe Game---");
+
+    //create a board of char array of size 10
+    static char[] board = new char[10];
+    public static void main(String[] args)
+    {
+        createEmptyBoard();
+
     }
+    /**UC1
+     * Name: createEmptyBoard
+     * Description: Static method to create empty board
+     * Algorithm: It takes board array indexes from 1 to 9 using for loop and
+     * assign empty space to each element in the board.
+     */
+    private static void createEmptyBoard()
+    {
+        for(int index=1; index<board.length;index++)
+        {
+            board[index] = ' ';
+        }
+    }
+
 }
