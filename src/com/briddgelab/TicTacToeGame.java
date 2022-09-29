959b821f3c950216040683bd58c5c7ec100b612e
nitis <nitish8152@gmail.com>
2022-09-29 16:23:40 +0530

    ChooseLetterToPlayGame

--- src/com/briddgelab/TicTacToeGame.java
+++ src/com/briddgelab/TicTacToeGame.java
@@ -1,12 +1,17 @@
 package com.briddgelab;
+import java.util.Scanner;
+
 //Class to create ticktactoe game with different methods//
 public class TicTacToeGame {
 
     //create a board of char array of size 10
     static char[] board = new char[10];
+    static char userLetter;
+    static char computerLetter;
     public static void main(String[] args)
     {
         createEmptyBoard();
+        chooseLetter();
 
     }
     /**UC1
@@ -22,5 +27,17 @@ public class TicTacToeGame {
             board[index] = ' ';
         }
     }
-
+    /**UC2
+     * Name: chooseLetter
+     * Description: static method to choose letter for user and computer
+     * Algorithm: It takes input from the user.Ternary operator(condition?exp1:exp2) lets us write if else
+     * statement in one line. if user letter is 'X' then computer letter is become 'O' otherwise it becomes 'X'
+     */
+    private static void chooseLetter()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose a letter :: X or O : ");
+        userLetter = scanner.next().toUpperCase().charAt(0);
+        computerLetter = (userLetter == 'X') ? 'O' : 'X';
+    }
 }
